4cfa60304e346054f474af6ef335421f171115de
Mark Khairallah <your.email@example.com>
2026-09-18 01:19:07 -0400

    WIP: declared sync regs, edge detection not yet implemented

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -16,4 +16,37 @@ module spi_peripheral (
     output reg [7:0] en_reg_pwm_7_0,
     output reg [7:0] en_reg_pwm_15_8,
     output reg [7:0] pwm_duty_cycle
+    reg sclk_sync0, sclk_sync1, ncs_sync0, ncs_sync1, copi_sync0, copi_sync1;
 );
+
+
+    always @(posedge clk or negedge rst_n)begin
+        if (!rst_n) begin
+            sclk_sync0 <= 0;
+            sclk_sync1 <= 0;
+        end else begin
+            sclk_sync0 <=SCLK;
+            sclk_sync1 <=sclk_sync0;
+        end
+    end
+
+    always @(posedge clk or negedge rst_n)begin
+        if (!rst_n) begin
+            ncs_sync0 <= 0;
+            ncs_sync1 <= 0;
+        end else begin
+            ncs_sync0 <=nCS;
+            ncs_sync1 <=ncs_sync0;
+        end
+    end
+
+    always @(posedge clk or negedge rst_n)begin
+        if (!rst_n) begin
+            copi_sync0 <= 0;
+            copi_sync1 <= 0;
+        end else begin
+            copi_sync0 <=COPI;
+            copi_sync1 <=copi_sync0;
+        end
+    end
+
